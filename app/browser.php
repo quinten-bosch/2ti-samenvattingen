3812f68459210c8989cef514eac25e93b825d3b9
quinten-bosch <quinten.bosch@student.ucll.be>
2022-02-17 11:38:45 +0100

    Update browser.php

--- app/browser.php
+++ app/browser.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto+Slab">
     <!-- Milligram CSS -->
-    <link rel="stylesheet" href="../vendor/milligram.css">
-    <link rel="stylesheet" href="../vendor/dark.css">
+    <link rel="stylesheet" href="vendor/milligram.css">
+    <link rel="stylesheet" href="vendor/dark.css">
   <style>
   
   body {
