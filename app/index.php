d5105cc3af57573b48a50c17482528e52fe09618
quinten-bosch <quinten.bosch@student.ucll.be>
2022-02-26 14:30:11 +0100

    Updates

--- app/index.php
+++ app/index.php
@@ -6,9 +6,7 @@
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto+Slab">
 
 
-    <!-- Anynymous, gdpr compliant, usage monitor -->
-    <!-- VISA Tracking Code for 2ti.quintenbosch.be --><script>(function(v,i,s,a,t){v[t]=v[t]||function(){(v[t].v=v[t].v||[]).push(arguments)};if(!v._visaSettings){v._visaSettings={}}v._visaSettings[a]={v:'1.0',s:a,a:'1',t:t};var b=i.getElementsByTagName('body')[0];var p=i.createElement('script');p.defer=1;p.async=1;p.src=s+'?s='+a;b.appendChild(p)})(window,document,'//app-worker.visitor-analytics.io/main.js','a01e0730-9707-11ec-b589-901b0edac50a','va')</script><!-- VISA Tracking Code for 2ti.quintenbosch.be -->
-
+   
  <!-- Milligram CSS -->
  <link rel="stylesheet" href="app/vendor/milligram.css">
  <link rel="stylesheet" href="app/vendor/dark.css">
@@ -33,6 +31,8 @@
 
 </head>
 <body>
+     <!-- Anynymous, gdpr compliant, usage monitor -->
+    <!-- VISA Tracking Code for 2ti.quintenbosch.be --><script>(function(v,i,s,a,t){v[t]=v[t]||function(){(v[t].v=v[t].v||[]).push(arguments)};if(!v._visaSettings){v._visaSettings={}}v._visaSettings[a]={v:'1.0',s:a,a:'1',t:t};var b=i.getElementsByTagName('body')[0];var p=i.createElement('script');p.defer=1;p.async=1;p.src=s+'?s='+a;b.appendChild(p)})(window,document,'//app-worker.visitor-analytics.io/main.js','a01e0730-9707-11ec-b589-901b0edac50a','va')</script><!-- VISA Tracking Code for 2ti.quintenbosch.be -->
 
 
 <title> Samenvattingen 2TI </title>
